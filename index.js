3ffba50ba873b975f682bf21f025a6d59d33282b
user <user@Eno-ITs-MacBook-Pro-4.local>
2023-12-08 17:39:19 +0700

    Fix server

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const {Server} = require('socket.io');
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
-        // origin: true,
-        // credentials: true
+        origin: true,
+        credentials: true
     }
 });
 
