f0a56c5e3324e39a8a5302d17265fd910050858d
user <user@Eno-ITs-MacBook-Pro-4.local>
2023-12-08 18:21:42 +0700

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-const http = require('http');
-const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
 const {Server} = require('socket.io');
 
 const io = new Server(server, {
@@ -25,6 +25,6 @@ app.get('/', (req, res) =>{
      //console.log('coec');
 })
 
-server.listen(5000, () => {
-    console.log('listening on port 5000');
+server.listen(3000, () => {
+    console.log('listening on port 3000');
 })
